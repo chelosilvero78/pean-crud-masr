270685d296ac1c1674d8dacd557a7be349792a10
Archit Choudhury <architchoudhury@outlook.com>
2019-01-14 14:20:05 +0800

    Boilerplate for CRUD request on angular

--- frontend/src/app/users.service.ts
+++ frontend/src/app/users.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  uri = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get(`${this.uri}/users`);
+  }
+
+  getIssueById(id) {
+    return this.http.get(`${this.uri}/users/${id}`);
+  }
+
+  createUser() {
+
+  }
+
+  updateUser() {
+
+  }
+
+  deleteUser() {
+
+  }
+
 }
